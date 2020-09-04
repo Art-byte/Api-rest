3b1d3e621027537483b7ece2a941ef0c2bbb700c
Arturo <Arturo_pdrzbn@hotmail.com>
2020-09-04 10:33:46 -0500

    Ajuste de modulos

--- Front/crudFront/src/app/app.module.ts
+++ Front/crudFront/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-//import { ToastrModule } from 'ngx-toastr';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +27,7 @@ import { FormsModule } from '@angular/forms'
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    //ToastrModule.forRoot(),
+    ToastrModule.forRoot(),
     HttpClientModule,
     FormsModule
   ],
